e2c2237136d3a4db2ddaa8a5c17fb2baef3ef8bb
chaeyoung <rnldyal1027@naver.com>
2023-03-07 16:08:20 +0900

    강한결합, 약한결합 commit

--- src/Strong_Weak.java
+++ src/Strong_Weak.java
@@ -14,7 +14,7 @@ public class Strong_Weak {
         Member m1 = new Member();   //강한결합
     }
     public static void MemberUse2(Member m){
-        Member m2 = m;  //약한결합
+        Member m2 = m;  //약한결합  //느슨하다. -> 스프링: 인터페이스 사용
     }
 }
 class Member{
@@ -22,3 +22,8 @@ class Member{
     String nickName;
     public Member(){};
 }
+
+//Cat c = new cat();
+//Animal c2 = new dog();
+//부모의 참조변수로 자식객체를 생성한다.
+//->좋은점 : dog 만 바꾸면 됨.
